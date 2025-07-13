5d53bd44b3bf92fca809160ecf200eca7514b968
Lior Kirshner <liorkk7@gmail.com>
2025-07-13 14:28:56 +0300

    ex2 - bank

---
+++ js-and-algo/closures_modules/Bank.js
@@ -0,0 +1,19 @@
+const Bank = function () {
+  let _money = 500;
+  const depositCash = function (cash) {
+    _money += cash;
+    return _money;
+  };
+  const checkBalance = function () {
+    return console.log(_money);
+  };
+  return {
+    deposit: depositCash,
+    showBalance: checkBalance,
+  };
+};
+
+const bank = Bank();
+bank.deposit(200);
+bank.deposit(250);
+bank.showBalance(); //should print 950
