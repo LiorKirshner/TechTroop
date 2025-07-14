2d563886cc07e7ad7de1a27102d14bf48f845c67
LiorKirshner <liorkk7@gmail.com>
2025-07-14 21:56:06 +0300

    dest

---
+++ js-and-algo/destructuring-spread-nullish/destructuring.js
@@ -0,0 +1,5 @@
+let facts = { numPlanets: 8, yearNeptuneDiscovered: 1846 };
+//let { numPlanets, yearNeptuneDiscovered } = facts;
+
+console.log(facts); // ?
+console.log(facts.yearNeptuneDiscovered); // ?
