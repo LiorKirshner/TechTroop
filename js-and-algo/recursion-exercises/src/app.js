cd7f24bb9a584d250fb0b213c4caeaa00d89f8b1
LiorKirshner <liorkk7@gmail.com>
2025-07-14 16:01:39 +0300

    Added recursion-lesson-exercises as internal folder

---
+++ js-and-algo/recursion-exercises/src/app.js
@@ -0,0 +1,28 @@
+/*
+  Write your code in the corresponding method
+  Please note: You must also add the correct arguments to the methods
+*/
+
+//Exercise 1
+const findFactorial = function() {
+  //Your code here
+
+}
+
+//Exercise 2
+const reverseString = function() {
+  //Your code here
+
+}
+
+//Exercise 3
+const arr1 = [1, 2, 3]
+const arr2 = []
+
+const swap = function() {
+  //Your code here
+
+}
+
+/* DO NOT REMOVE THE EXPORTS BELOW */
+module.exports = { findFactorial, reverseString, swap }
