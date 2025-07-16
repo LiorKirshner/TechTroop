a89b9ba06fa1ad84f8da259993cfd743628b6986
LiorKirshner <liorkk7@gmail.com>
2025-07-16 18:24:13 +0300

    ex2

---
+++ js-and-algo/jesting-around/Excrcises.js
@@ -0,0 +1,13 @@
+//should return true if n is even, false otherwise
+function isEven(n) {
+  return n % 2 == 0 ? true : false;
+}
+//should remove at least one element from the array `arr`
+function //should remove at least one element from the array `arr`
+removeAtLeastOne(arr) {
+  let numItemsToRemove = Math.floor(Math.random() * (arr.length - 1)) + 1;
+  arr.splice(0, numItemsToRemove);
+  return arr;
+}
+
+module.exports = { isEven, removeAtLeastOne };
---
+++ js-and-algo/jesting-around/Excrcises.test.js
@@ -0,0 +1,41 @@
+//const { describe } = require("node:test");
+//const { describe } = require("node:test");
+const { isEven, removeAtLeastOne } = require("./Excrcises");
+
+describe("should return true if n is even, false otherwise", () => {
+  test("returns true for even number", () => {
+    expect(isEven(42)).toBeTruthy();
+  });
+
+  test("returns false for odd number", () => {
+    expect(isEven(41)).toBeFalsy();
+  });
+});
+
+describe("removeAtLeastOne", () => {
+  test("removes at least one element from a non-empty array", () => {
+    let arr = [1, 23, 4, 32, 4, 4, 4, 5454, 4655, 6, 3, 3, 5, 6, 4, 4, 4];
+    let len = arr.length;
+    let arr2 = removeAtLeastOne(arr);
+    expect(len).toBeGreaterThan(arr2.length);
+  });
+
+  test("removes the only element from an array of length 1", () => {
+    let arr = [7];
+    let arr2 = removeAtLeastOne(arr);
+    expect(arr2.length).toBe(0);
+  });
+
+  test("returns empty array when input is empty", () => {
+    let arr = [];
+    let arr2 = removeAtLeastOne(arr);
+    expect(arr2.length).toBe(0);
+  });
+
+  test("does not mutate the original array", () => {
+    let arr = [1, 2, 3, 4];
+    let arrCopy = [...arr];
+    removeAtLeastOne(arr);
+    expect(arr).not.toEqual(arrCopy);
+  });
+});
