2b8c7ed769b824a43c7e88d21e927cb08e72b20f
LiorKirshner <liorkk7@gmail.com>
2025-07-19 16:15:18 +0300

    OOP Inheritance

--- js-and-algo/oop/school.js
+++ js-and-algo/oop/school.js
@@ -1,5 +1,5 @@
 class Person {
-  constructor(name = null, startYear) {
+  constructor(name, startYear) {
     this.name = name;
     this.startYear = startYear;
     this.courses = [];
@@ -33,7 +33,7 @@ class Student extends Person {
 
 class Teacher extends Person {
   constructor(name, startYear, salary) {
-    super(startYear);
+    super(name, startYear);
     this.salary = salary;
     this.name = "Professor " + name;
     this.courses = {};
@@ -48,8 +48,63 @@ class Teacher extends Person {
   }
 }
 
+class TeachingAssistant extends Teacher {
+  constructor(name, startYear, salary) {
+    super(name, startYear, salary);
+  }
+}
+
+class Principal extends Person {
+  constructor(name, startYear) {
+    super(name, startYear);
+    this.teachers = [];
+    this.students = [];
+  }
+  hireTeacher(teacher) {
+    this.teachers.push(teacher);
+    console.log(this.name + "jest hired " + teacher.name);
+  }
+  recruitStudent(student) {
+    this.students.push(student);
+  }
+  expelStudent(student) {
+    const index = this.students.indexOf(student);
+    if (index !== -1) {
+      this.students.splice(index, 1);
+    }
+  }
+  transferStudent(student, principal) {
+    this.expelStudent(student);
+    principal.recruitStudent(student);
+  }
+}
+
+const p1 = new Principal("Martin", 1991);
+const p2 = new Principal("Martha", 1990);
+
 const t1 = new Teacher("Cassandra", 2002, 40000);
-t1.addCourse("Algebra II");
-t1.addCourse("Algebra II");
-t1.addCourse("Trigonometry");
-console.log(t1.courses); //should print {Algebra II: 2, Trigonometry: 1}
+const t2 = new Teacher("Kevin", 2006, 30000);
+
+const s1 = new Student("Ronda", 2017);
+const s2 = new Student("Byron", 2016);
+
+//1 & 2
+p1.hireTeacher(t1); //should print "Martin just hired Cassandra"
+console.log(p1.teachers); //should print Array(1) [Teacher] - the teacher should be Cassandra
+
+p1.hireTeacher(t2); //should print "Martin just hired Kevin"
+console.log(p1.teachers); //should print Array(2) [Teacher, Teacher]
+
+//3 & 4
+p1.recruitStudent(s1);
+p1.recruitStudent(s2);
+console.log(p1.students); //should print Array(2) [Student, Student]
+
+//5
+p1.expelStudent(s1);
+console.log(p1.students); //should print Array(1) [Student] - the student should be Byron
+
+//6
+p1.transferStudent(s2, p2);
+console.log(p1.students); //should print Array(0) []
+console.log(p2.students); //should print Array(1) [Student] - the student should be Byron
