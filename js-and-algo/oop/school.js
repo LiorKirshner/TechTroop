c699f3082de5086a2fe8088a8c4a675a65b880dc
LiorKirshner <liorkk7@gmail.com>
2025-07-19 15:31:37 +0300

    teacher inheritance addcourse function

--- js-and-algo/oop/school.js
+++ js-and-algo/oop/school.js
@@ -1,5 +1,5 @@
 class Person {
-  constructor(name, startYear) {
+  constructor(name = null, startYear) {
     this.name = name;
     this.startYear = startYear;
     this.courses = [];
@@ -16,6 +16,13 @@ class Student extends Person {
     this.grades = [];
   }
 
+  //method overriding!
+  addCourse(course) {
+    if (this.courses.indexOf(course) == -1) {
+      super.addCourse(course);
+    }
+  }
+
   receiveGrade(courseName, finalGrade) {
     this.grades.push({
       course: courseName,
@@ -23,23 +30,26 @@ class Student extends Person {
     });
   }
 }
+
 class Teacher extends Person {
   constructor(name, startYear, salary) {
-    super(name, startYear);
-    this.salary = this.salary;
+    super(startYear);
+    this.salary = salary;
+    this.name = "Professor " + name;
+    this.courses = {};
   }
+
   giveGrade(student, courseName, grade) {
     student.receiveGrade(courseName, grade);
   }
+  //method overriding!
+  addCourse(courseName) {
+    this.courses[courseName] = (this.courses[courseName] || 0) + 1;
+  }
 }
 
-const s1 = new Student("Ronda", 2017);
 const t1 = new Teacher("Cassandra", 2002, 40000);
-
-t1.giveGrade(s1, "Algebra II", 82);
-const firstGrade = s1.grades[0];
-
-console.log(
-  `${s1.name} received an ${firstGrade.grade} in ${firstGrade.course}`
-);
-//above should print "Ronda received an 82 in Algebra II"
+t1.addCourse("Algebra II");
+t1.addCourse("Algebra II");
+t1.addCourse("Trigonometry");
+console.log(t1.courses); //should print {Algebra II: 2, Trigonometry: 1}
