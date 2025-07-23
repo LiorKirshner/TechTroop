9651c3036bf376db57e677924b354effc7147213
LiorKirshner <liorkk7@gmail.com>
2025-07-23 20:11:34 +0300

    finish The Best Game Ever

--- web-dev-js/basic-dom-master/main.js
+++ web-dev-js/basic-dom-master/main.js
@@ -1,1 +1,46 @@
-// console.log(document)
+const move = function (direction) {
+  const ball = document.getElementById("ball");
+  const computedStyle = window.getComputedStyle(ball);
+  let index;
+
+  if (direction === "left" || direction === "right") {
+    index = parseInt(computedStyle.left) || 0;
+    index += direction === "right" ? 15 : -15;
+    ball.style.left = index + "px";
+  } else {
+    index = parseInt(computedStyle.top) || 0;
+    index += direction === "down" ? 15 : -15;
+    ball.style.top = index + "px";
+  }
+};
+
+const header = document.createElement("h1");
+header.innerHTML = "The Best Game Ever";
+header.style.color = "#c0392b";
+header.style.fontFamily = "Helvetica";
+document.body.appendChild(header);
+
+const subHeader = document.createElement("h2");
+subHeader.setAttribute("class", "sub-header");
+subHeader.innerHTML = "by Lior ";
+header.appendChild(subHeader);
+
+const right = document.getElementById("right");
+right.onclick = function () {
+  move("right");
+};
+
+const left = document.getElementById("left");
+left.onclick = function () {
+  move("left");
+};
+
+const up = document.getElementById("up");
+up.onclick = function () {
+  move("up");
+};
+
+const down = document.getElementById("down");
+down.onclick = function () {
+  move("down");
+};
