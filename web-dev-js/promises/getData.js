e3a3c044c32999997b91b6b3a93609e581a6390a
LiorKirshner <liorkk7@gmail.com>
2025-07-28 16:26:28 +0300

    basic data

---
+++ web-dev-js/promises/getData.js
@@ -0,0 +1,17 @@
+function getData() {
+  return new Promise((resolve, reject) => {
+    fetch("https://example.com/data")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Request failed");
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
+}
