b4e4c5b14064b02332fd6e028e1a86dcd3d71612
LiorKirshner <liorkk7@gmail.com>
2025-07-29 23:36:52 +0300

    your function to search by whatever queryType is (ISBN or title)

--- web-dev-js/AJAX/main.js
+++ web-dev-js/AJAX/main.js
@@ -1,14 +1,14 @@
-const fetchBook = function (ISBN) {
+const fetch = function (queryType, queryValue) {
   $.ajax({
     method: "GET",
-    url: `https://www.googleapis.com/books/v1/volumes?q=isbn:${ISBN}`,
+    url: `https://www.googleapis.com/books/v1/volumes?q=${queryType}:${queryValue}`,
     success: function (data) {
       const book = data.items?.[0]?.volumeInfo;
       if (!book) {
         $("body").append(`<p>Book not found.</p>`);
         return;
       }
-
+      console.log(data.items?.[0]?.volumeInfo?.title);
       $("body").append(
         `<p>${data.items?.[0]?.volumeInfo?.title || "No title found"}</p>`
       );
@@ -16,11 +16,11 @@ const fetchBook = function (ISBN) {
   });
 };
 
-function handleClick() {
-  const isbn = $("#isbn-input").val();
-  if (!isbn) {
-    alert("Please enter an ISBN.");
-    return;
-  }
-  fetchBook(isbn);
-}
+// function handleClick() {
+//   const isbn = $("#isbn-input").val();
+//   if (!isbn) {
+//     alert("Please enter an ISBN.");
+//     return;
+//   }
+//   fetchBook(isbn);
+// }
