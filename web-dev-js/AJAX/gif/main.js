addee1fee8ed1df557e9bcfd93a9c46d32d4c10d
LiorKirshner <liorkk7@gmail.com>
2025-07-30 00:34:37 +0300

    made a simple gif generator

--- web-dev-js/AJAX/gif/main.js
+++ web-dev-js/AJAX/gif/main.js
@@ -1,15 +1,20 @@
-const searchTerm = "cats";
+$(document).ready(function () {
+  $("#searchBTN").click(function () {
+    const searchTerm = $("#search").val();
 
-$.ajax({
-  method: "GET",
-  url: `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}&limit=1`,
-  success: function (data) {
-    const gifUrl = data.data[0].embed_url;
-    $("#gif-container").html(
-      `<iframe src="${gifUrl}" frameborder="0"></iframe>`
-    );
-  },
-  error: function () {
-    $("#gif-container").text("😿 שגיאה בטעינת ה־GIF");
-  },
+    $.ajax({
+      method: "GET",
+      url: `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}&limit=1`,
+      success: function (data) {
+        const gifUrl = data.data[0].embed_url;
+
+        $("#gif-container").append(
+          `<iframe src="${gifUrl}" frameborder="0"></iframe>`
+        );
+      },
+      error: function () {
+        $("#gif-container").text("😿 שגיאה בטעינת ה־GIF");
+      },
+    });
+  });
 });
