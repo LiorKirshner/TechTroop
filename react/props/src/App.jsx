95579c3bfbb8f72ac6559bf4b3e8d8e09f4bf1e5
LiorKirshner <liorkk7@gmail.com>
2025-08-05 16:22:57 +0300

    sc3

--- react/props/src/App.jsx
+++ react/props/src/App.jsx
@@ -3,14 +3,23 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Company from "./component/Company";
+
 function App() {
+  const upperCase = (name) => name.toUpperCase();
+
   let companies = [
     { name: "Tesla", revenue: 140 },
     { name: "Microsoft", revenue: 300 },
     { name: "Google", revenue: 600 },
   ];
 
-  return <Company name={companies[0].name} revenue={companies[0].revenue} />;
+  return (
+    <div>
+      {companies.map((c) => (
+        <Company name={c.name} revenue={c.revenue} key={c.name} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
