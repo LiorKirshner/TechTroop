c58f05a1cebc0e854a08729e04e77630f3ba8515
LiorKirshner <liorkk7@gmail.com>
2025-08-06 18:05:29 +0300

    done

--- react/input/src/App.jsx
+++ react/input/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import Exercise1 from "./components/Exercise1";
+import Exercise2 from "./components/Exercise2";
 
 function App() {
   return (
     <>
-      <Exercise1 />
+      <div className="ex">
+        <h4>Ex1</h4>
+        <Exercise1 />
+      </div>
+      <div className="ex">
+        <h4>Ex2</h4>
+        <Exercise2 />
+      </div>
     </>
   );
 }
