945bab9eb919eedf4b611ff8a4ef20b8713b6893
LiorKirshner <liorkk7@gmail.com>
2025-09-10 16:16:19 +0300

    Retrieve only the name and height for all the healthy dolphins sorted by their height (tallest to shortest).

--- backend/SQL-intro/solutions/ex_6.sql
+++ backend/SQL-intro/solutions/ex_6.sql
@@ -1,1 +1,4 @@
 USE sql_intro;
+
+-- Retrieve only the name and height for all the healthy dolphins sorted by their height (tallest to shortest)
+SELECT name, height FROM Dolphin WHERE healthy = 1 ORDER BY height DESC;
